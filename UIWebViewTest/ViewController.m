d06ee56c1e876ee162df2ffb739a9ca3769115c1
kingleekong <konglee873@gmail.com>
2016-03-31 09:28:51 +0800

    测试WebView 并去除背景灰色系

--- UIWebViewTest/ViewController.m
+++ UIWebViewTest/ViewController.m
@@ -16,7 +16,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]];
+    
+    [webView loadRequest:request];
+    
+    UIButton *buttonBack = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    buttonBack.frame = CGRectMake(20, 100, 200, 50);
+    
+    [buttonBack setTitle:@"后退" forState:UIControlStateNormal];
+    
+    buttonBack.backgroundColor = [UIColor orangeColor];
+    
+    [buttonBack addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    
+//    [webView addSubview:buttonBack];
+    
+    UIButton *buttonFroward = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    buttonFroward.frame = CGRectMake(20, 400, 200, 50);
+    
+    [buttonFroward setTitle:@"前进" forState:UIControlStateNormal];
+    
+    buttonFroward.backgroundColor = [UIColor orangeColor];
+    
+    [buttonFroward addTarget:self action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+    
+//    [webView addSubview:buttonFroward];
+    
+    for (UIScrollView* view in webView.subviews)
+    {
+        if ([view isKindOfClass:[UIScrollView class]])
+        {
+            view.bounces = NO;
+        }
+    }
+    
+    webView.backgroundColor = [UIColor clearColor];
+    
+    [webView setOpaque:NO];
+    
+    [self.view addSubview:webView];
+    
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning {
