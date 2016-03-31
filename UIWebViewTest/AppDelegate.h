d7bf3dc1a4c8ed3a5ed430b8a5f108c3040d1681
kingleekong <konglee873@gmail.com>
2016-03-31 09:06:10 +0800

    Initial Commit

---
+++ UIWebViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIWebViewTest
+//
+//  Created by kong on 16/3/31.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIWebViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIWebViewTest
+//
+//  Created by kong on 16/3/31.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
